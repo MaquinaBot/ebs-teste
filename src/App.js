a1d5754dd48e52fb76d30ec09e110f8bde6c7303
Ederson Almeida <ederson.almeida@maquinabot.com>
2023-01-31 15:43:25 -0300

    teste 02

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testando o Branch de test
+          teste 02
         </p>
         <a
           className="App-link"
