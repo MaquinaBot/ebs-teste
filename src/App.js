189b8fe1a11282b2e73a53df492b2b380dff4d3a
Ederson Almeida <ederson.almeida@maquinabot.com>
2023-01-31 13:06:04 -0300

    primeira alteração

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste de deploy do Elastic BeansTalk
+          Teste de deploy do Amplify
         </p>
         <a
           className="App-link"
